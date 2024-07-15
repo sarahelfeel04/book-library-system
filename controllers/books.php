4ae74974865441c9708b2426bfcfd0ad94fb2e1a
Sarah Elfeel <sarah.elfeel@robustastudio.com>
2024-07-15 13:37:28 +0300

    feat: create a books page for the library

---
+++ controllers/books.php
@@ -0,0 +1,10 @@
+<?php
+
+$config = require('config.php');
+$db = new Database($config['database']);
+
+$heading = 'My Books';
+
+$books = $db->query('select * from books')->get();
+
+require "views/books.view.php";
