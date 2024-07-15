fc1c8129fc3574527fe95d1e8d815d2ec1c18b5a
Sarah Elfeel <sarah.elfeel@robustastudio.com>
2024-07-15 13:55:50 +0300

    refactor: create function validate for new books

--- controllers/book-add.php
+++ controllers/book-add.php
@@ -7,7 +7,31 @@ $heading = 'Add Book';
 $errors = [];
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    
+    validate ();
 
+    if (empty($errors)) {
+        $db->query('INSERT INTO books(title, author, publishing_date, cover_image, summary) VALUES(:title, :author, :publishing_date, :cover_image, :summary)', [
+            'title' => $_POST['title'],
+            'author' => $_POST['author'],
+            'publishing_date' => $_POST['publishing_date'],
+            'cover_image' => $_POST['cover_image'],
+            'summary' => $_POST['summary']
+        ]);
+
+        header('Location: /books');
+        exit;
+    }
+
+}
+
+function displayError($errors, $field) {
+    if (isset($errors[$field])) {
+        echo '<p class="text-red-600">' . $errors[$field] . '</p>';
+    }
+}
+
+function validate(){
     // Validate input fields for a book
     if (strlen($_POST['title']) === 0) {
         $errors['title'] = 'Book Name is required';
@@ -32,25 +56,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (strlen($_POST['cover_image']) === 0) {
         $errors['cover_image'] = 'Cover Image URL is required';
     }
-
-    
-    if (empty($errors)) {
-        $db->query('INSERT INTO books(title, author, publishing_date, cover_image, summary) VALUES(:title, :author, :publishing_date, :cover_image, :summary)', [
-            'title' => $_POST['title'],
-            'author' => $_POST['author'],
-            'publishing_date' => $_POST['publishing_date'],
-            'cover_image' => $_POST['cover_image'],
-            'summary' => $_POST['summary']
-        ]);
-
-        header('Location: /books');
-        exit;
-    }
-
 }
 
-function displayError($errors, $field) {
-    if (isset($errors[$field])) {
-        echo '<p class="text-red-600">' . $errors[$field] . '</p>';
-    }
-}
+require 'views/book-add.view.php';
