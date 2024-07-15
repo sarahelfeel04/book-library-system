6423c05b9eaae4db1098dc369b9c53c5712f18bf
Sarah Elfeel <sarah.elfeel@robustastudio.com>
2024-07-15 13:24:54 +0300

    feat: create website layout

---
+++ views/partials/head.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en" class="h-full bg-gray-100">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <style>
+        .aspect-w-1 { aspect-ratio: 1 / 1; }
+        .aspect-h-1 { aspect-ratio: 1 / 1; }
+    </style>
+    <title>Librart</title>
+    <script src="https://cdn.tailwindcss.com?plugins=forms"></script>
+</head>
+
+<body class="h-full">
+    <div class="min-h-full">
