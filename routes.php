05306815088018e16690659ca2ff282eed14adbf
Sarah Elfeel <sarah.elfeel@robustastudio.com>
2024-07-15 13:57:26 +0300

    feat: include new routes

--- routes.php
+++ routes.php
@@ -2,5 +2,9 @@
 
 return [
     '/' => 'controllers/index.php',
+    '/about' => 'controllers/about.php',
+    '/contact' => 'controllers/contact.php',
     '/books' => 'controllers/books.php',
+    '/book' => 'controllers/book.php',
+    '/books/add' => 'controllers/book-add.php',
 ];
