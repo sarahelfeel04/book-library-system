3da8183ece6b4d70f63791a8cad895955c480aba
Sarah Elfeel <sarah.elfeel@robustastudio.com>
2024-07-15 14:25:31 +0300

    feat: adjust form to take picture from file as input

--- controllers/book-add.php
+++ controllers/book-add.php
@@ -58,4 +58,31 @@ function validate(){
     }
 }
 
+function fileUpload(){
+    if (isset($_FILES['cover_image']) && $_FILES['cover_image']['error'] === UPLOAD_ERR_OK) {
+        $fileTmpPath = $_FILES['cover_image']['tmp_name'];
+        $fileName = $_FILES['cover_image']['name'];
+        $fileSize = $_FILES['cover_image']['size'];
+        $fileType = $_FILES['cover_image']['type'];
+        $fileNameCmps = explode(".", $fileName);
+        $fileExtension = strtolower(end($fileNameCmps));
+
+        $allowedfileExtensions = array('jpg', 'gif', 'png');
+        if (in_array($fileExtension, $allowedfileExtensions)) {
+            // Directory in which the uploaded file will be moved
+            $uploadFileDir = 'uploads/';
+            $dest_path = $uploadFileDir . strlen($_POST['title']);
+
+            if (move_uploaded_file($fileTmpPath, $dest_path)) {
+                echo 'File is successfully uploaded.';
+                // Save the file path to the database along with other book details
+            } else {
+                echo 'There was some error moving the file to upload directory';
+            }
+        } else {
+            echo 'Upload failed. Allowed file types: ' . implode(',', $allowedfileExtensions);
+        }
+    }
+}
+
 require 'views/book-add.view.php';
